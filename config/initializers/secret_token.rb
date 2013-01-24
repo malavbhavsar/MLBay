aa45082af5577e736950b85ea023b17880e27ec2
malavbhavsar <malav.bhavsar@gmail.com>
2013-01-24 02:31:16 -0500

    Add basic jruby rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MLBay::Application.config.secret_token = 'f77ea22c70350ece30923eff6d91dc56cbc24da7b14a2356aa65a6e4ed6f1380cb6d1d5232df6d09c51f14109fe0128a2e4db3b4225b162026fdb87fa6e7582b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MLBay::Application.config.session_store :cookie_store, key: '_MLBay_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MLBay::Application.config.session_store :active_record_store
